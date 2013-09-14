61bd1fab58dfb126d900aa6c8103557446aec736
kedebug <sunweiqq@gmail.com>
2013-09-14 13:22:15 +0800

    001

--- 001_Last/last.hs
+++ 001_Last/last.hs
@@ -1,3 +1,12 @@
 myLast :: [a] -> a
 myLast [x] = x
 myLast (_:xs) = myLast xs
+
+-- const x _ = x
+-- const id :: b -> a -> a
+myLast' = foldr1 (const id)  
+myLast'' = foldr1 (flip const)
+
+myLast''' = head . reverse
+
+myLast'''' x = x !! (length x - 1)
