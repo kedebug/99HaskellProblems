1fb8abf4c45fec06a07c63b379f3204f93096feb
kedebug <sunweiqq@gmail.com>
2013-09-14 11:59:49 +0800

    001

--- 001_Last/last.hs
+++ 001_Last/last.hs
@@ -1,3 +1,12 @@
 myLast :: [a] -> a
+myLast [] = error "No end for empty list"
 myLast [x] = x
 myLast (_:xs) = myLast xs
+
+-- const :: a -> b -> a
+-- const x _ = x
+
+myLast' :: [a] -> a
+myLast' = foldr1 (const id)
+
+
